1f04917d6ed4234457f659d80243649d5b712ad7
Klik461 <klikolik461@gmail.com>
2023-05-18 18:57:00 +0300

    Test Checking The Basket With Selected Accessories Passed

--- src/test/java/elements/HomePageElements.java
+++ src/test/java/elements/HomePageElements.java
@@ -13,4 +13,6 @@ public class HomePageElements extends AbstractBasePage {
     public static final String BTN_FOLLOWING_TWITTER = "//a[@aria-label='Twitter']";
     public static final String BTN_FOLLOWING_YOUTUBE = "//a[@aria-label='Youtube']";
     public static final String BTN_FOLLOWING_INSTAGRAM = "//a[@aria-label='Instagram']";
+    public static final String BTN_SHOP_BMW_PARTS_AND_ACCESSORIES =
+            "//div[contains(@class,'__nav-holder')]//a[contains(text(),'Accessories')]";
 }
