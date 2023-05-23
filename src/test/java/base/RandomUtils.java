80d22e785f1166db2ef1a17177805152c503aa95
Tania Stanko <taniastanko@Tanias-Air.lan>
2023-05-23 16:01:02 +0300

    Sdst-9 refactor random

--- src/test/java/base/RandomUtils.java
+++ src/test/java/base/RandomUtils.java
@@ -3,6 +3,7 @@ package base;
 import java.security.SecureRandom;
 
 public class RandomUtils {
+
     public static int randomNum(int size) {
         return new SecureRandom().nextInt(size);
     }
