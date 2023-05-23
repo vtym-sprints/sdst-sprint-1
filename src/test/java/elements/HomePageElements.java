17ca9e8d76372271aecbb9fe103c13f9f7e6f48a
Tania Stanko <taniastanko@Tanias-Air.lan>
2023-05-23 16:00:11 +0300

    Sdst-7 new changes

--- src/test/java/elements/HomePageElements.java
+++ src/test/java/elements/HomePageElements.java
@@ -13,10 +13,10 @@ public class HomePageElements extends AbstractBasePage {
     public static final String BTN_FOLLOWING_TWITTER = "//a[@aria-label='Twitter']";
     public static final String BTN_FOLLOWING_YOUTUBE = "//a[@aria-label='Youtube']";
     public static final String BTN_FOLLOWING_INSTAGRAM = "//a[@aria-label='Instagram']";
-    public final static String GLOBAL_POPUP_DISMISS_BTN_SEL = "//button[contains(text(), 'Dismiss')]";
-    public final static String BUILD_YOUR_OWN_BUTTON = "//a[@analytics-event='topnav.byo.byo.link']";
+    public static final String GLOBAL_POPUP_DISMISS_BTN_SEL = "//button[contains(text(), 'Dismiss')]";
+    public static final String BUILD_YOUR_OWN_BUTTON = "//a[@analytics-event='topnav.byo.byo.link']";
     public static final String BTN_SHOP_BMW_PARTS_AND_ACCESSORIES = "//div[contains(@class,'__nav-holder')]//a[contains(text(),'Accessories')]";
-    public static final String FOOTER_MENU = "//div[@class='accordion-base__trigger label-1--bold']",
-            BUILD_YOUR_OWN = "//a[@analytics-event='footer-shopping-tools.build-your-own.build-your-own.link']";
+    public static final String FOOTER_MENU = "//div[@class='accordion-base__trigger label-1--bold']";
+    public static final String BUILD_YOUR_OWN = "//a[@analytics-event='footer-shopping-tools.build-your-own.build-your-own.link']";
 
 }
