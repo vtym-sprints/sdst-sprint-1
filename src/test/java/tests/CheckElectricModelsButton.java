3b7bb8cde577fa0641c80ab36b07ed86a86d8e71
Vitya <podill1122333@gmail.com>
2023-05-25 12:49:47 +0300

    upgrade branch44

--- src/test/java/tests/CheckElectricModelsButton.java
+++ src/test/java/tests/CheckElectricModelsButton.java
@@ -11,7 +11,6 @@ public class CheckElectricModelsButton extends AbstractBaseTest {
 
     @Test
     public void CheckElectricModelsButton() {
-
         HomePage homePage =new HomePage(driver);
 
         homePage
