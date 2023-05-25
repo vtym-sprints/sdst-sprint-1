c31c5e016c6e9834f9f820ca1695bcfb7dbe0415
Tania Stanko <taniastanko@Tanias-MacBook-Air.local>
2023-05-25 21:33:51 +0300

    Sdst-11 test done

---
+++ src/test/java/tests/BMWConnectedDriveTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import base.AbstractBaseTest;
+import org.testng.annotations.Test;
+import pages.BMWConnectedDrivePage;
+import pages.HomePage;
+
+import static elements.BMWConnectedDriveElements.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BMWConnectedDriveTest extends AbstractBaseTest {
+
+    @Test
+    public void checkDownloadingFiles() throws Exception {
+        HomePage homePage = new HomePage(driver);
+        BMWConnectedDrivePage bmwConnectedDrivePage = new BMWConnectedDrivePage(driver);
+
+        homePage
+                .dismissPopup()
+                .goToBMWConnectedDrivePage();
+
+        bmwConnectedDrivePage
+                .clickSeeNewFeatures()
+                .clickDownloadTrafficCameraInformation()
+                .clickDownloadRemoteEngineStart()
+                .clickDownloadParkingAssistantProfessional()
+                .isTillLoader();
+
+        String firstFilename = bmwConnectedDrivePage.readPdfContent(filePathTrafficCameraInformation);
+        assertThat(firstFilename)
+                .contains("TRAFFIC CAMERA INFORMATION");
+
+        bmwConnectedDrivePage.deleteFile(filePathTrafficCameraInformation);
+
+        String secondFileName = bmwConnectedDrivePage.readPdfContent(filePathRemoteEngineStart);
+        assertThat(secondFileName)
+                .contains("REMOTE ENGINE");
+
+        bmwConnectedDrivePage.deleteFile(filePathRemoteEngineStart);
+
+        String thirdFileName = bmwConnectedDrivePage.readPdfContent(filePathParkingAssistantProfessional);
+        assertThat(thirdFileName)
+                .contains("PARKING");
+
+        bmwConnectedDrivePage.deleteFile(filePathParkingAssistantProfessional);
+    }
+}
